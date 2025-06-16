254d90f6d10d7161e90bd2e0184aee88633f236f
El0 <100817854+ElGrant000@users.noreply.github.com>
2025-06-16 23:34:00 +1000

    Update high.php

--- vulnerabilities/exec/source/high.php
+++ vulnerabilities/exec/source/high.php
@@ -1,37 +1,28 @@
 <?php
 
-if( isset( $_POST[ 'Submit' ]  ) ) {
-	// Get input
-	$target = trim($_REQUEST[ 'ip' ]);
+if (isset($_POST['Submit'])) {
+    // Get input
+    $target = trim($_REQUEST['ip']);
 
-	// Set blacklist
-	$substitutions = array(
-		'||' => '',
-		'&'  => '',
-		';'  => '',
-		'| ' => '',
-		'-'  => '',
-		'$'  => '',
-		'('  => '',
-		')'  => '',
-		'`'  => '',
-	);
+    // Validate IP address format (whitelisting)
+    if (filter_var($target, FILTER_VALIDATE_IP)) {
 
-	// Remove any of the characters in the array (blacklist).
-	$target = str_replace( array_keys( $substitutions ), $substitutions, $target );
+        // Escape argument to prevent command injection
+        $safe_target = escapeshellarg($target);
 
-	// Determine OS and execute the ping command.
-	if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
-		// Windows
-		$cmd = shell_exec( 'ping  ' . $target );
-	}
-	else {
-		// *nix
-		$cmd = shell_exec( 'ping  -c 4 ' . $target );
-	}
+        // Determine OS and execute the ping command safely
+        if (stristr(php_uname('s'), 'Windows NT')) {
+            // Windows
+            $cmd = shell_exec('ping ' . $safe_target);
+        } else {
+            // *nix
+            $cmd = shell_exec('ping -c 4 ' . $safe_target);
+        }
 
-	// Feedback for the end user
-	$html .= "<pre>{$cmd}</pre>";
+        // Feedback for the end user
+        $html .= "<pre>{$cmd}</pre>";
+    } else {
+        $html .= "<pre>Invalid IP address.</pre>";
+    }
 }
-
 ?>
