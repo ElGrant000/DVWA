0407f4a492de8d08af360014f2e80d40df67894c
El0 <100817854+ElGrant000@users.noreply.github.com>
2025-06-16 23:35:39 +1000

    Update low.php

--- vulnerabilities/exec/source/low.php
+++ vulnerabilities/exec/source/low.php
@@ -1,21 +1,28 @@
 <?php
 
-if( isset( $_POST[ 'Submit' ]  ) ) {
-	// Get input
-	$target = $_REQUEST[ 'ip' ];
+if (isset($_POST['Submit'])) {
+    // Get input
+    $target = trim($_REQUEST['ip']);
 
-	// Determine OS and execute the ping command.
-	if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
-		// Windows
-		$cmd = shell_exec( 'ping  ' . $target );
-	}
-	else {
-		// *nix
-		$cmd = shell_exec( 'ping  -c 4 ' . $target );
-	}
+    // Validate input: only allow valid IPv4 or IPv6 addresses
+    if (filter_var($target, FILTER_VALIDATE_IP)) {
 
-	// Feedback for the end user
-	$html .= "<pre>{$cmd}</pre>";
+        // Escape the IP address argument to safely pass to shell
+        $safe_target = escapeshellarg($target);
+
+        // Determine OS and execute the ping command
+        if (stristr(php_uname('s'), 'Windows NT')) {
+            // Windows
+            $cmd = shell_exec('ping ' . $safe_target);
+        } else {
+            // *nix
+            $cmd = shell_exec('ping -c 4 ' . $safe_target);
+        }
+
+        // Feedback for the end user
+        $html .= "<pre>{$cmd}</pre>";
+    } else {
+        $html .= "<pre>Invalid IP address.</pre>";
+    }
 }
-
 ?>
