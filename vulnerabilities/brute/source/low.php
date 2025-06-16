87a91001a7b1efa57a69f2cab195716a9024ff6e
El0 <100817854+ElGrant000@users.noreply.github.com>
2025-06-16 23:37:34 +1000

    Update low.php

--- vulnerabilities/brute/source/low.php
+++ vulnerabilities/brute/source/low.php
@@ -1,32 +1,21 @@
 <?php
 
-if( isset( $_GET[ 'Login' ] ) ) {
-	// Get username
-	$user = $_GET[ 'username' ];
+if (isset($_GET['Login'])) {
+    // Get input
+    $user = $_GET['username'];
+    $pass = $_GET['password'];
+    $pass = md5($pass);
 
-	// Get password
-	$pass = $_GET[ 'password' ];
-	$pass = md5( $pass );
+    // Create a prepared statement
+    $stmt = mysqli_prepare($GLOBALS["___mysqli_ston"], "SELECT * FROM users WHERE user = ? AND password = ?");
+    mysqli_stmt_bind_param($stmt, 'ss', $user, $pass); // 'ss' = two strings
 
-	// Check the database
-	$query  = "SELECT * FROM `users` WHERE user = '$user' AND password = '$pass';";
-	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+    // Execute and fetch result
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
 
-	if( $result && mysqli_num_rows( $result ) == 1 ) {
-		// Get users details
-		$row    = mysqli_fetch_assoc( $result );
-		$avatar = $row["avatar"];
+    if ($result && mysqli_num_rows($result) === 1) {
+        $row = mysqli_fetch_assoc($result);
+        $avatar = htmlspecialchars($row["avatar"]); // Prevent XSS
 
-		// Login successful
-		$html .= "<p>Welcome to the password protected area {$user}</p>";
-		$html .= "<img src=\"{$avatar}\" />";
-	}
-	else {
-		// Login failed
-		$html .= "<pre><br />Username and/or password incorrect.</pre>";
-	}
-
-	((is_null($___mysqli_res = mysqli_close($GLOBALS["___mysqli_ston"]))) ? false : $___mysqli_res);
-}
-
-?>
+        $html .= "<p>Welcome to the password protected a
